0a355ed0d9148a1846acbdfa6acc4921accbfd98
Lindsay Spencer <43455549+lindsayspencer@users.noreply.github.com>
2019-01-03 21:32:01 -0600

    fixed handles on page 2

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -8,8 +8,8 @@ const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>About this site</h1>
-    <h3><i class="twitter icon"></i> @lindscatspencer</h3>
-    <h3><i class="github icon"></i> @lindsayspencer</h3>
+    <h4>Twitter <i class="twitter icon"></i> @lindscatspencer</h4>
+    <h4>Github <i class="github icon"></i> @lindsayspencer</h4>
     <p>Built with React.js and Gatsby.js, this site creates 1 - 5 paragraphs (small, medium, or large) of randomly generated text. Barkorem Ipsuruff is based on the original Lorem Ipsum text, with 'bark' replacing the first letter of some words, and 'ruff' replacing the last letter of others. Made for dog lovers, but all can enjoy! :)</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
